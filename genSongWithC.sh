95482096a40b3dd619061951f637ea5b564f5099
szk2s <suzukisjss@gmail.com>
2019-06-29 15:55:40 +0900

    Fix minor issue
    Add double quotation

--- genSongWithC.sh
+++ genSongWithC.sh
@@ -4,7 +4,7 @@ performance_rnn_generate \
 --config=multiconditioned_performance_with_dynamics \
 --bundle_file=$(pwd)/models/multiconditioned_performance_with_dynamics.mag \
 --output_dir=$(pwd)/tmp \
---pitch_class_histogram=[1,0,0,0,0,0,0,0,0,0,0,0] \
+--pitch_class_histogram="[1,0,0,0,0,0,0,0,0,0,0,0]" \
 --notes_per_second=12 \
 --num_outputs=1 \
 --num_steps=5000 \
